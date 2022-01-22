34639fd3fc2d72fef8377579ec4d9da7c39c4489
Salvador Oliveira <slsalvadorlucas@gmail.com>
2022-01-21 21:40:58 -0400

    add pokecard component

---
+++ src/Pokecard.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class Pokecard extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Pokecard!</h2>
+      </div>
+    );
+  }
+}
+
+export default Pokecard;
