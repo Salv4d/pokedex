f36c3b6a0ad0e7f84f770c89e46d54ffe0b69dec
Salvador Oliveira <slsalvadorlucas@gmail.com>
2022-01-24 21:12:04 -0400

    add alt to images

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -11,15 +11,15 @@ class Pokecard extends Component {
 
   render() {
     const { id, name, type, base_experience, idx } = this.props;
-
+    const POKE_API =
+      "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/";
     return (
       <div className="Pokecard" key={idx}>
         <h2 className="Pokecard-name">{name}</h2>
         <img
           className="Pokecard-img"
-          src={`https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${this.normalizeId(
-            id
-          )}.png`}
+          src={`${POKE_API}${this.normalizeId(id)}.png`}
+          alt={`${name}`}
         />
         <div className="Pokecard-props">
           <p className="Pokecard-type">Type: {this.capitalize(type)}</p>
